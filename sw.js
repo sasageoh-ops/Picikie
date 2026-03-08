a42dc465d61af5c07188c233a624d3b7aa37461a
sasageoh-ops <sasageoh@gmail.com>
2026-03-09 00:26:58 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'poptastic-v2';
+const assets = ['index.html', 'manifest.json', 'icon.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
